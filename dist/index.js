4ce14aa1458ad6fe878d46c8facfbc54661231ec
abrahanalarcon <moiseshazim@gmail.com>
2024-11-30 18:24:20 -0400

    Probando las rutas en postman

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const server_1 = require("./server");
+console.log('desde index.ts');
+console.log('despues de sumar');
+console.log('que duro');
+(0, server_1.sumar)();
+//# sourceMappingURL=index.js.map
