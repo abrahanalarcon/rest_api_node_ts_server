f165deb212c4f453b53fd220727e3680b94968b4
abrahanalarcon <moiseshazim@gmail.com>
2024-11-30 18:35:41 -0400

    separando las rutas en su propio archivo

---
+++ src/router.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express'
+
+const router = Router()
+// Routing
+router.get('/', (req, res) => {
+    
+    res.json('Desde GET')
+})
+router.post('/', (req, res) => {
+    
+    res.json('Desde post')
+})
+router.put('/', (req, res) => {
+    
+    res.json('Desde put')
+})
+router.patch('/', (req, res) => {
+    
+    res.json('Desde patch')
+})
+
+router.delete('/', (req, res) => {
+    
+    res.json('Desde delete')
+})
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,8 @@
 import express from 'express'
+import router from './router'
  
 const server = express()
 
-// Routing
-server.get('/', (req, res) => {
-    
-    res.json('Desde GET')
-})
-server.post('/', (req, res) => {
-    
-    res.json('Desde post')
-})
-server.put('/', (req, res) => {
-    
-    res.json('Desde put')
-})
-server.patch('/', (req, res) => {
-    
-    res.json('Desde patch')
-})
-
-server.delete('/', (req, res) => {
-    
-    res.json('Desde delete')
-})
+server.use('/api/productos',router)
 
 export default server
